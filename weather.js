3cc310826d8b9f0bed689c8642b28bddc2ed3f5a
J. Cardenas <jonscardenas@gmail.com>
2018-03-05 16:10:29 -0600

    weather js cleanup

--- weather.js
+++ weather.js
@@ -5,36 +5,24 @@ function hasTouch() {
            || navigator.msMaxTouchPoints > 0;
 };
 
-
+//API key
 var APPID = "23f36ca1bfadd2adc206be6c15bf9147";
-//var temperature;
-//var zipSubmit = false;
-//var tempIsF = true;
-
-//var zipInput = 68128;
-//var cityInput = "seattle";
-
-
-
 
 function getWeather(input){
 
-	var zipCodeRegEx = /\d{5}/;
-	var zipCodeTest = zipCodeRegEx.test(input);
-  console.log("zip test: " + zipCodeTest);
-
-	var urlCityCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + input + ",us&units=imperial&APPID=" + APPID;
-
+  var zipCodeRegEx = /\d{5}/;
+  var zipCodeTest = zipCodeRegEx.test(input);
+	
+  var urlCityCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + input + ",us&units=imperial&APPID=" + APPID;
   var urlZipCurrent = "https://api.openweathermap.org/data/2.5/weather?zip=" + input + ",us&units=imperial&APPID=" + APPID;
  
-  
   if (zipCodeTest && input.length == 5){
-  	console.log("Input is a valid zip code!");
+    //console.log("Input is a valid zip code!");
     sendRequest(urlZipCurrent);
   }else{
-  	console.log("non zip code entered");
-  	sendRequest(urlCityCurrent);
-  }
+    //console.log("non zip code entered");
+    sendRequest(urlCityCurrent);
+  };
   
 }
 
@@ -43,19 +31,16 @@ function sendRequest(url){
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function(){
   	
-    console.log("Status: " + xmlhttp.status);
+    //console.log("Status: " + xmlhttp.status);
   
     if (xmlhttp.readyState == 4 && xmlhttp.status ==200){
       //var data = xmlhttp.responseText;
       var data = JSON.parse(xmlhttp.responseText);
-      console.log(data);
+      //console.log(data);
       
       //location
-      //var zipCode = zip;
       var cityName = data.name;
-      
-      //console.log("Zip Code: " + zipCode);
-      console.log("City: " + cityName);
+      //console.log("City: " + cityName);
       
       //weather
       var currentTemp = Math.floor(data.main.temp);
@@ -68,20 +53,16 @@ function sendRequest(url){
       var wind = Math.floor(data.wind.speed);
       
       
-      console.log("Current Temp: " + currentTemp);
+      /*console.log("Current Temp: " + currentTemp);
       console.log("High: " + tempHigh);
       console.log("Low: " + tempLow);
       console.log("Humidity: " + humidity);
       console.log("Conditions: " + conditions);
       console.log("weather ID: " + weatherId);
       console.log("Pressure: " + pressure);
-      console.log("Wind: " + wind);
-      
-      //weatherBackground(957)
+      console.log("Wind: " + wind);*/
       
       weatherBackground(cityName, currentTemp, tempHigh, tempLow, humidity, conditions, weatherId, pressure, wind)
-      
-      
     };
     
   };
@@ -91,7 +72,7 @@ function sendRequest(url){
   
   xmlhttp.onloadend = function() {
     if(xmlhttp.status == 404){
-    	console.log("Please enter valid input");
+    	//console.log("Please enter valid input");
       
       //set invalid text for 1s and shake
       $("#submit-button").effect("shake", {distance:5});
@@ -106,40 +87,39 @@ function sendRequest(url){
 
 
 function weatherBackground(cityInput, currentTempInput, highInput, lowInput, humidityInput, conditionsInput, weatherInput, pressureInput, windInput){
-	console.log("weather input: " + weatherInput);
+  //console.log("weather input: " + weatherInput);
   $(".WeatherBackground").css("width", "0%");
-  //$("#weather-background-container").css("filter", "blur(0px)");
   
   if (weatherInput>=200 && weatherInput<= 232){
-  	console.log("Weather is: Storm");
+  	//console.log("Weather is: Storm");
     $("#weather-background-storm").css("width", "100%");
     
   }else if ((weatherInput >= 300 && weatherInput <= 531) || (weatherInput == 701)){
-  	console.log("Weather is: Rain");
+  	//console.log("Weather is: Rain");
     $("#weather-background-rain").css("width", "100%");
   
   }else if (weatherInput >= 600 && weatherInput <= 622){
-  	console.log("Weather is: Snow");
+  	//console.log("Weather is: Snow");
     $("#weather-background-snow").css("width", "100%");
   
   }else if (weatherInput >= 801 && weatherInput <= 804){
-  	console.log("Weather is: Cloudy");  
+  	//console.log("Weather is: Cloudy");  
     $("#weather-background-cloudy").css("width", "100%");
   
   }else if (weatherInput >=711 && weatherInput <=781){
-    	console.log("Weather is: obscurred and hazy?");
+    	//console.log("Weather is: obscurred and hazy?");
       $("#weather-background-haze").css("width", "100%");
   
   }else if ((weatherInput >= 900 && weatherInput <= 906 && weatherInput != 905) || (weatherInput == 957) || (weatherInput == 962)){
-  	console.log("Weather is: EXTREME!!");
+  	//console.log("Weather is: EXTREME!!");
     $("#weather-background-extreme").css("width", "100%");
   
   }else if ((weatherInput>= 951 && weatherInput <= 956) || (weatherInput == 905)){
-  	console.log("Weather is: windy");
+  	//console.log("Weather is: windy");
     $("#weather-background-windy").css("width", "100%");
   
   }else{
-  	console.log("weather is: clear");
+  	//console.log("weather is: clear");
     $("#weather-background-clear").css("width", "100%");
   }
   
@@ -150,10 +130,7 @@ function weatherBackground(cityInput, currentTempInput, highInput, lowInput, hum
   $("#input-container").css("transform", "translateY(30vh)");
   
   //Display Weather data
-  /*cityInput, currentTempInput, highInput, lowInput, humidityInput, conditionsInput, weatherInput*/
-  //$("#output-container-main").css("visibility", "visible");
   $("#output-container-main").css({"visibility": "visible", "opacity": "1"});
-  //$(".OutputContainer").css("opacity", "1");
       
   $("#city-name").text(cityInput);    
   $("#current-temp").html(currentTempInput + "&deg;F");
@@ -173,15 +150,15 @@ function weatherBackground(cityInput, currentTempInput, highInput, lowInput, hum
 $(document).ready(function(){
 	//adds HasHover class to body to allow for hovering over elements
 	if (!hasTouch()) {
-		console.log("Not a touch screen, adding hover class!");
-    		$("body").addClass("HasHover");
+	  //console.log("Not a touch screen, adding hover class!");
+    	  $("body").addClass("HasHover");
 	}
 	
 	
 	//return new width upon resize
 	$(window).resize(function() {
   	var width = $(window).width(); 
-  	console.log(width);
+  	//console.log(width);
 	});
   
   
@@ -190,23 +167,15 @@ $(document).ready(function(){
   	//console.log("button clicked");
     event.preventDefault();
     var $userInput = $("#user-input").val();
-    console.log("!!User Input: " + $userInput);
+    //console.log("!!User Input: " + $userInput);
     
     if ($userInput != ""){
-    	console.log("User Input: " + $userInput);
+    	//console.log("User Input: " + $userInput);
       getWeather($userInput); 
     }else{
       $("#submit-button").effect("shake", {distance:5});
     }
   });
-  
-  /*$("#user-input").keyup(function(e) {
-         if(e.which == 13) {
-             e.preventDefault();
-             console.log('Enter key hit');
-             //$("#submit-button").click();
-          }
-    });*/
 	
     $("#submit-button").hover(
   function() {
